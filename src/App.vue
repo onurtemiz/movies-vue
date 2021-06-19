0780b9095006941a66eae461f4fb7a0506453e0d
Onur Temiz <onurmain@gmail.com>
2021-06-19 23:51:44 +0300

    Init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col class="col-12">
+            <v-sheet min-height="100px" rounded="lg" class="mt-5">
+              <v-row justify="center">
+                <v-col class="col-3">
+                  <v-text-field
+                    label="Search"
+                    v-model="state.search"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            </v-sheet>
+          </v-col>
+          <v-col class="col-12">
+            <v-sheet rounded="lg">
+              <v-container class="d-flex flex-wrap justify-center">
+                <movie-item
+                  v-for="movie in movies"
+                  :key="movie.id"
+                  :movie="movie"
+                ></movie-item>
+              </v-container>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive, toRefs } from '@vue/composition-api';
+import MovieItem from './components/MovieItem.vue';
+import useSWRV from 'swrv';
+import { api } from './utils';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    MovieItem,
+  },
+  name: 'app',
+  setup() {
+    const state = reactive({
+      search: 'fight',
+    });
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    const getMovies = async function(url) {
+      try {
+        const { data } = await api.get(url);
+        return Promise.resolve(data.Search);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    };
+
+    const { data: movies, error } = useSWRV(
+      () => `/?s=${state.search}&y=2020`,
+      getMovies
+    );
+
+    return {
+      movies: movies,
+      error,
+      state,
+      ...toRefs(state),
+    };
+  },
+};
+</script>
